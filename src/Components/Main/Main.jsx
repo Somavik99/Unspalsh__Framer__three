4e6da6cd41d948aa66c0eabd2188c174ed48cd00
Avik Roy <somavik99@gmail.com>
2023-08-11 03:57:42 +0530

    main

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,6 +1,34 @@
+import { useEffect, useState } from "react";
 import Images from "../../Components/ComponentVariants/ImageData";
+import { BsArrowLeft, BsArrowRight } from "react-icons/bs";
 
 const Main = () => {
+  const [CurrentIndex, setCurrentIndex] = useState(0);
+
+  const PreviousImage = (prev) => {
+    setCurrentIndex(CurrentIndex === 0 ? Images.length - 1 : prev - 1);
+  };
+
+  const NextImage = (next) => {
+    setCurrentIndex(CurrentIndex === Images.length - 1 ? 0 : next + 1);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      return setCurrentIndex((n) => {
+        n > CurrentIndex
+          ? Images[CurrentIndex]
+          : n === Images.length - 1
+          ? 0
+          : n + 1;
+      });
+    }, 2000);
+
+    return () => {
+      clearTimeout(interval);
+    };
+  }, [CurrentIndex]);
+
   return (
     <div style={{ boxShadow: " 0 0 2rem  -2rem   tomato" }}>
       <h1
@@ -24,22 +52,33 @@ const Main = () => {
           alignItems: "center",
         }}
       >
-        {Images.map((img, i) => {
-          return (
-            <img
-              key={i}
-              src={img.image}
-              alt="mountains"
-              style={{
-                width: "68vw",
-                height: "80vh",
-                borderRadius: "20px",
-                margin: "5%",
-                boxShadow: "5px 5px 0.5rem 0.55rem rgba(0.5,0.5,0.5,0.4) ",
-              }}
-            />
-          );
-        })}
+        <button onClick={PreviousImage}>
+          <BsArrowLeft />
+        </button>
+        {Array.isArray(Images) &&
+          Images.map((img, i) => {
+            return (
+              <div key={i}>
+                {i === CurrentIndex && (
+                  <img
+                    src={img.image}
+                    alt="mountains"
+                    style={{
+                      width: "68vw",
+                      height: "80vh",
+                      borderRadius: "20px",
+                      margin: "5%",
+                      boxShadow:
+                        "5px 5px 0.5rem 0.55rem rgba(0.5,0.5,0.5,0.4) ",
+                    }}
+                  />
+                )}
+              </div>
+            );
+          })}
+        <button onClick={NextImage}>
+          <BsArrowRight />
+        </button>
       </div>
     </div>
   );
